cea72bd5b90c0a853fad5e0093f24c6ec9dc8d64
Alex Souza <aasouzaconsult@gmail.com>
2021-09-22 21:00:09 -0300

    Exemplos em R

--- R.R
+++ R.R
@@ -50,25 +50,6 @@ dep = data.frame(lista)
 
 subset(dep, sexoDependente == "F")
 
-##############
-## MONGO DB ##
-##############
-install.packages("mongolite")
-
-library(mongolite)
-
-connection_string = 'mongodb://localhost'
-
-cli <- mongo("tbcliente")
-
-alldata <- cli$find('{}')
-
-print(alldata)
-
-# Filtrando (>=) 
-clifil <- cli$find('{"dscpf" : { "$gte" : 11111111112 } }')
-print(clifil)
-
 # mais sobre R
 # https://analisereal.com/2017/02/07/data-frames/
 # https://ycaro.net/2019/04/conectar-no-banco-de-dados-mysql-pela/
