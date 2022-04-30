73aa13ebef750ac75ee2742248297b94eaf1ca2a
Alex Souza <aasouzaconsult@gmail.com>
2022-04-30 14:00:56 -0300

    scripts
    R

--- scripts/3. R/R.R
+++ scripts/3. R/R.R
@@ -1,5 +1,6 @@
 # pacotes
-install.packages("RMySQL"); 
+#install.packages("RMySQL");
+
 library(DBI)
 library(RMySQL)
 
@@ -14,28 +15,51 @@ dbListTables(con)
 dbListFields(con, 'tbproduto')
 
 # Comando SELECT
-query_sql = dbSendQuery(con, "SELECT * FROM tbproduto where cdPro < 8;")
+query_sql = dbSendQuery(con, "SELECT * FROM tbcliente;")
 
-#query_sql = dbSendQuery(con, "select * from vwvendas_resumida;")
-
-#query_sql = dbSendQuery(con, "select *
-#                                from tbvendas  ven
-#                                join tbcliente cli on cli.cdCli = ven.cdCli
-#                                join tbproduto pro on pro.cdPro = ven.cdPro;")
+query_sql = dbSendQuery(con, "select *
+                                from tbvendas  ven
+                                join tbcliente cli on cli.cdCli = ven.cdCli
+                                join tbproduto pro on pro.cdPro = ven.cdPro;")
 
 lista = fetch(query_sql, n=-1)
 
 # Vendo o resultado 
 df = data.frame(lista)
 
+df
+
 # comandos R
 colnames(df)
+
+# Consultando com Joins
+# ---------------------
+query_sql = dbSendQuery(con, "select *
+                                from tbvendas  ven
+                                join tbcliente cli on cli.cdCli = ven.cdCli
+                                join tbproduto pro on pro.cdPro = ven.cdPro;")
+
+# ver o tipo do dado
+# S4 - tipo personalizado
+class(query_sql)
+
+lista = fetch(query_sql, n=-1)
+
+# Vendo o resultado 
+df = data.frame(lista)
+
+df
 
 # Somatorio de valores unitarios
 sum(df$vrUni)
 
 #desconecta da base
 dbDisconnect(con)
+
+# para criar uma tabela seria: 
+# RJDBC::dbWriteTable(conexao,'<>nome_tabela', <>data_frame)
+# Mais informações: https://blogdozouza.files.wordpress.com/2022/04/rjdbc.pdf
+
 
 # mais um teste
 # -------------
